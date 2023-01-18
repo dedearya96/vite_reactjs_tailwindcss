72f2d2fa8efcf437fbc889d292370131df5329fc
dedearya96 <arya.widnyana96@@gmail.com>
2023-01-18 04:37:16 +0000

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="bg-gray-900 flex items-center justify-center min-h-screen">
+            <div className=" bg-gray-800 border-t border-gray-600 rounded-lg max-w-lg w-full p-6 text-center">
+                <h4 className=" text-white text-2xl">Hello React + Vite</h4>
+                <p className=" text-lg text-gray-400 leading-relaxed">Javascript library</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
